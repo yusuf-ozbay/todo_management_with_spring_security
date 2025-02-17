f76196c0c51b47eaae32af70a5805072598989bc
yusuf-ozbay <ozbay9985@gmail.com>
2025-02-17 16:36:19 +0300

    pom.xml dosyasına gerekli dependency'ler eklendi

--- src/main/java/org/example/todo_management_with_spring_security/TodoManagementWithSpringSecurityApplication.java
+++ src/main/java/org/example/todo_management_with_spring_security/TodoManagementWithSpringSecurityApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class TodoManagementWithSpringSecurityApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(TodoManagementWithSpringSecurityApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(TodoManagementWithSpringSecurityApplication.class, args);
+    }
 
 }
