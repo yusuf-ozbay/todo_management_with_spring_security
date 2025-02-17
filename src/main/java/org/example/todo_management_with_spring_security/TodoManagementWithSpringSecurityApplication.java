109292eef81d717fb13f691331da8f1284e849fb
yusuf-ozbay <ozbay9985@gmail.com>
2025-02-17 15:55:19 +0300

    first commit

---
+++ src/main/java/org/example/todo_management_with_spring_security/TodoManagementWithSpringSecurityApplication.java
@@ -0,0 +1,13 @@
+package org.example.todo_management_with_spring_security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoManagementWithSpringSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoManagementWithSpringSecurityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/todo_management_with_spring_security/TodoManagementWithSpringSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.todo_management_with_spring_security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoManagementWithSpringSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
