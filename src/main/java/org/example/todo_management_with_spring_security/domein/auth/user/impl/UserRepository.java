c1512fdff86202b7acd94c41426e14b4a6b8771e
yusuf-ozbay <ozbay9985@gmail.com>
2025-02-18 22:25:10 +0300

    user ve Todo için repository sınıfları yazıldı

--- src/main/java/org/example/todo_management_with_spring_security/domein/auth/user/impl/UserRepository.java
+++ src/main/java/org/example/todo_management_with_spring_security/domein/auth/user/impl/UserRepository.java
@@ -1,4 +1,6 @@
 package org.example.todo_management_with_spring_security.domein.auth.user.impl;
 
-public interface UserRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User , String> {
 }
--- src/main/java/org/example/todo_management_with_spring_security/domein/todo/impl/TodoRepository.java
+++ src/main/java/org/example/todo_management_with_spring_security/domein/todo/impl/TodoRepository.java
@@ -1,4 +1,11 @@
 package org.example.todo_management_with_spring_security.domein.todo.impl;
 
-public interface TodoRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+
+public interface TodoRepository extends JpaRepository<Todo,String>, JpaSpecificationExecutor<Todo> {
+
+    //JpaSpesificationExecuter dinamik sorgular oluşturmak için kullanılır (sorguları runtime'da oluşturulup çalıştırılabilir).
+
+
 }
