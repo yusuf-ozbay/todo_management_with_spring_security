59cec71a2ca90b9132166f896533b698b22bfb8d
yusuf-ozbay <ozbay9985@gmail.com>
2025-02-19 22:51:34 +0300

    CreateUserRequest kullanıcının giriş yapması için gereken bilgiler değiken olarak yazıldı

--- src/main/java/org/example/todo_management_with_spring_security/domein/auth/user/web/CreateUserRequest.java
+++ src/main/java/org/example/todo_management_with_spring_security/domein/auth/user/web/CreateUserRequest.java
@@ -1,4 +1,12 @@
 package org.example.todo_management_with_spring_security.domein.auth.user.web;
 
+
+import org.example.todo_management_with_spring_security.domein.auth.user.impl.enums.Role;
+
+import java.util.Set;
+
 public class CreateUserRequest {
+    public String username;
+    public String password;
+    public Set<Role> authorities;
 }
