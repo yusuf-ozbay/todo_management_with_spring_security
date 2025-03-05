6aa505747e400abd6ca9b6d90c80c5b29bcc4a9c
yusuf-ozbay <ozbay9985@gmail.com>
2025-03-05 16:44:34 +0300

    Kullanıcı yetkilerini saklamak için Authorities entity'si oluşturuldu.

--- src/main/java/org/example/todo_management_with_spring_security/domein/auth/authhorities/Authorities.java
+++ src/main/java/org/example/todo_management_with_spring_security/domein/auth/authhorities/Authorities.java
@@ -1,4 +1,32 @@
 package org.example.todo_management_with_spring_security.domein.auth.authhorities;
 
-public class Authorities {
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.example.todo_management_with_spring_security.library.rest.AbstractEntity;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@Entity
+@Table(name = Authorities.TABLE)
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@EntityListeners(AuditingEntityListener.class)
+public class Authorities extends AbstractEntity {
+    public static final String TABLE = "authorities";
+    private static final String USERNAME = "username";
+    private static final String AUTHORITY = "authority";
+
+    @Column(name = USERNAME)
+    private String username;
+
+    @Column(name = AUTHORITY)
+    private String authority;
+
 }
